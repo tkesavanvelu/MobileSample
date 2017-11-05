29be7a017a982dbc81e770953274bea9459e055d
kv_developer <kv_developer@users.noreply.github.com>
2017-11-04 21:06:59 -0500

    sample:wq

---
+++ App1/App1/App1/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "kesavan velu's first app"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
